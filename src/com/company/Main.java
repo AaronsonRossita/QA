ac02eab11a80a0d48d3493be436ce43344744261
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-01-24 21:07:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    // DATA TYPES
+    // int        1
+    // double     1.5
+    // boolean    true/false
+    // char       'a'
+
+    // String     "Word dsgsdkgj 39489284 89^&*^&*("
+
+
+
+//      psvm
+    public static void main(String[] args) {
+
+        int x = 5;
+        double y = 5.36;
+        boolean b1 = true;
+        boolean b2 = false;
+        char c = 'a';
+
+        String s1 = "my string is number 1";
+
+//      sout
+        System.out.println("main");
+
+/*        System.out.println(x);
+        System.out.println(y);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(c);
+
+        System.out.println(s1); */
+
+
+
+    }
+
+}
+
