66a692b355b1b0b83687bb5a84a30333c033012e
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-01-27 21:31:27 +0200

    1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,16 +15,18 @@ public class Main {
 //      psvm
     public static void main(String[] args) {
 
-        int x = 5;
+        int x = 7;
+        int x1;
+
         double y = 5.36;
         boolean b1 = true;
         boolean b2 = false;
         char c = 'a';
 
-        String s1 = "my string is number 1";
+//        String s1 = "my string is number 1";
 
 //      sout
-        System.out.println("main");
+//        System.out.println("main");
 
 /*        System.out.println(x);
         System.out.println(y);
@@ -34,7 +36,49 @@ public class Main {
 
         System.out.println(s1); */
 
+        x1 = 8;
 
+//        System.out.println("int x1 = " + x1);
+//        System.out.println("double y = " + y);
+//        System.out.println("boolean b1 = " + b1);
+//        System.out.println("boolean b2 = " + b2);
+//        System.out.println("char c = " + c);
+        int a = 5;
+        int b = 5;
+
+        int age = 21;
+
+//        if(a >= b){
+//            System.out.println("a is bigger than b");
+//        }
+//
+//        if (age > 17){
+//            System.out.println("you can drink");
+//        }else{
+//            System.out.println("you're a child go home");
+//        }
+//
+//        System.out.println("I made to the end");
+
+//        if (a > b){
+//            System.out.println("a is bigger than b");
+//        }else{
+//            System.out.println("b is bigger than a");
+//        }
+//
+//        if (a == 5){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+
+        if (a == 7){
+            System.out.println("seven");
+        }else if (a > 7){
+            System.out.println("bigger than seven");
+        }else{
+            System.out.println("smaller than seven");
+        }
 
     }
 
